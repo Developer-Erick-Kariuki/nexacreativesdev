6276a491f0c8eef457734975256bcaa96b5ec642
erick <simplexerick@gmail.com>
2025-02-14 14:43:22 +0300

    chages to the blogsite

--- frontend/blog/BlogPost.jsx
+++ frontend/blog/BlogPost.jsx
@@ -27,7 +27,7 @@ const serializers = {
         default:
           return (
             <p
-              style={{ textAlign: "justify" }}
+              style={{ textAlign: "left" }}
               className="leading-loose text-right"
             >
               {props.children}
@@ -65,43 +65,57 @@ const BlogPost = () => {
   if (!SinglePost) return <p>Loading...</p>;
 
   return (
-    <section className="text-white px-6 md:px-10 max-w-7xl mx-auto pt-4">
-      <Header />
-      <div className="flex flex-col md:flex-row w-full mt-20 justify-center gap-8">
-        {SinglePost.map((current) => (
-          <div key={current.customId}>
-            <img src={current.imageUrl} width={600} alt={current.title} />
-            <p className="mt-4 text-accent">
-              {moment(current.publishedAt).format("MMMM, YYYY")}
-            </p>
-            <h1 className="text-3xl mt-3 max-w-md font-bold">
-              {current.title}
-            </h1>
-            <div className="max-w-lg mt-6 text-justify">
-              <SanityBlockContent
-                blocks={current.body}
-                serializers={serializers}
-              />
+    <>
+      <section className="text-white px-6 md:px-10 mx-auto pt-4">
+        <Header />
+        <div className="flex justify-center">
+          <div className="flex items-start gap-8 flex-col md:flex-row mt-20">
+            {SinglePost.map((current) => (
+              <div className="flex flex-col" key={current.customId}>
+                <img
+                  className="bg-slate-900"
+                  src={current.imageUrl}
+                  width={600}
+                  alt={current.title}
+                />
+                <p className="mt-4 text-accent">
+                  {moment(current.publishedAt).format("MMMM, YYYY")}
+                </p>
+                <h1 className="text-3xl mt-3 max-w-lg font-bold">
+                  {current.title}
+                </h1>
+                <div className="max-w-lg mt-6 text-justify">
+                  <SanityBlockContent
+                    blocks={current.body}
+                    serializers={serializers}
+                  />
+                </div>
+              </div>
+            ))}
+
+            <div className="flex flex-col w-1/4  gap-6">
+              {posts.map((post) => (
+                <div key={post.title} className="flex max-w-xs flex-col">
+                  <img
+                    className="mx-8"
+                    src={post.imageUrl}
+                    width={200}
+                    alt={post.title}
+                  />
+                  <Link to={`/blog/BlogPost/${post.customId}`}>
+                    <h2 className="text-lg font-bold mt-2">{post.title}</h2>
+                  </Link>
+                  <p className="mt-2 text-accent">
+                    {moment(post.publishedAt).format("MMMM, YYYY")}
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
-        ))}
-
-        <div className="flex flex-col gap-6">
-          {posts.map((post) => (
-            <div key={post.title} className="flex max-w-sm flex-col">
-              <img className="mx-8" src={post.imageUrl} alt={post.title} />
-              <Link to={`/blog/BlogPost/${post.customId}`}>
-                <h2 className="text-2xl font-bold mt-2">{post.title}</h2>
-              </Link>
-              <p className="mt-2 text-accent">
-                {moment(post.publishedAt).format("MMMM, YYYY")}
-              </p>
-            </div>
-          ))}
         </div>
-      </div>
+      </section>
       <Footer />
-    </section>
+    </>
   );
 };
 
