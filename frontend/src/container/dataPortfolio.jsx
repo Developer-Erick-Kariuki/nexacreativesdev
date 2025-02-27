90711608b2b22285a9b0c87691f3aa53242a2f69
erick <simplexerick@gmail.com>
2025-02-27 16:22:16 +0300

    added all categories for quick view

--- frontend/src/container/dataPortfolio.jsx
+++ frontend/src/container/dataPortfolio.jsx
@@ -55,7 +55,7 @@ const Portfolioa = () => {
 
       {/* Filter Buttons */}
       <ul className="flex w-full flex-wrap  justify-center gap-2 mt-8 md:gap-8">
-        {["Social Media", "Branding", "Print Design", "Web Design"].map(
+        {["All", "Social Media", "Branding", "Print Design", "Web Design"].map(
           (item, index) => (
             <li
               key={index}
